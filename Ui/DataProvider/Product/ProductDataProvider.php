04bbf736614f76be0867e4ad1edd53d9eb7c5885
cuongth <cuongth@magenest.com>
2023-07-14 12:05:19 +0700

    Section 4 - Exercise 1

--- Ui/DataProvider/Product/ProductDataProvider.php
+++ Ui/DataProvider/Product/ProductDataProvider.php
@@ -3,13 +3,16 @@
 namespace Magenest\CourseType\Ui\DataProvider\Product;
 
 use Magento\Framework\Api\Filter;
+use Magento\Catalog\Model\ResourceModel\Product\CategoryLink\ProductCollection as CategoryProductCollection;
 
 class ProductDataProvider extends \Magento\Catalog\Ui\DataProvider\Product\ProductDataProvider
 {
     public function addFilter(Filter $filter)
     {
         if ($filter->getField() == 'category_id') {
-            $this->getCollection()->addCategoriesFilter(array('in' => $filter->getValue()));
+            /** @var CategoryProductCollection $categoryProductCollection */
+            $categoryProductCollection = $this->getCollection();
+            $categoryProductCollection->addCategoriesFilter(['in' => $filter->getValue()]);
         } else if (isset($this->addFilterStrategies[$filter->getField()])) {
             $this->addFilterStrategies[$filter->getField()]
                 ->addFilter(
